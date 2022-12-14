373af87342267534748cd2fdfa6b88e908fd289e
yungwenpeng <yungwenpeng@gmail.com>
2022-12-14 11:45:23 +0800

    Add update user function

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -13,6 +13,12 @@ import Typography from '@mui/material/Typography';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import ErrorIcon from '@mui/icons-material/Error';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -28,6 +34,13 @@ function UsersList({ setCurrentPath }) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const websocket = useRef(null); //useRef doesn't triggers rerenders
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+    const [userOldInfo, setUserOldInfo] = useState(null);
+    const [userNewName, setUserNewName] = useState(null);
+    const [userNewEmail, setUserNewEmail] = useState(null);
+    const [userNewPassword, setUserNewPassword] = useState(null);
+    const [userNewRole, setUserNewRole] = useState(null);
+    const [postResult, setPostResult] = useState(null);
     //console.log('UsersList - loading:', loading, ' , data:', data, ' , error:', error);
 
     useEffect(() => {
@@ -83,6 +96,17 @@ function UsersList({ setCurrentPath }) {
                 setData([...data, message]);
             } else if (message['method'] === 'delete') {
                 setData(data.filter(user => user.id !== message['id']));
+            } else if (message['method'] === 'update') {
+                let newData =  data.map((user) => {
+                    if (user.id === message['id']){
+                        user.userName = message['userName'];
+                        user.email = message['email'];
+                        user.password = message['password'];
+                        user.role = message['role'];
+                    } 
+                    return user;
+                });
+                setData(newData);
             }
         };
         websocket.current = socket;
@@ -91,16 +115,64 @@ function UsersList({ setCurrentPath }) {
         };
     }, [data]);
 
-    const handleEdit = user => {
+    useEffect(() => {
+        if (postResult) {
+            setTimeout(() => {
+                setPostResult(null);
+            }, 1000)
+        }
+    }, [postResult])
 
+    const showEditDialog = user => {
+        console.log('Edit user:', user)
+        setUserOldInfo(user);
+        setOpenEditDialog(true);
     }
+    const handleEditSave = user => {
+        console.log('handleEditSave New : ', userNewName, ' , ', userNewEmail, ' , ', userNewPassword, ' , ', userNewRole);
+        if (userNewName === '' || userNewEmail === '' || userNewRole === '' || userNewPassword === '') {
+            setPostResult('Each item is required');
+            return;
+        } else if (userNewPassword === null || userNewPassword === undefined) {
+            setPostResult('Password is required');
+            return;
+        }
+        var editData = {
+            "userName": userNewName ? userNewName : user['userName'],
+            "email": userNewEmail ? userNewEmail : user['email'],
+            "password": userNewPassword ? userNewPassword : user['password'],
+            "role": userNewRole ? userNewRole : user['role']
+        }
+        console.log('handleEditSave - oldUser: ', user['email']);
+        console.log('handleEditSave - editData: ', JSON.stringify(editData));
+        try {
+            fetch(api_url + '/api/users/' + user['email'], {
+                method: 'PUT',
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }),
+                body: JSON.stringify(editData)
+            })
+                .then(() => console.log('Update successful'));
+            setOpenEditDialog(false);
+            setUserOldInfo(null);
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            console.log('Update finally');
+        }
+    }
+    const handleEditClose = () => {
+        setOpenEditDialog(false);
+        setUserOldInfo(null);
+    };
 
     const handleDelete = userEmail => {
         setData(data.filter(user => user.email !== userEmail));
         try {
             fetch(api_url + '/api/users/' + userEmail, { method: 'DELETE', })
                 .then(() => console.log('Delete successful'));
-
         } catch (err) {
             console.log(err.message);
         } finally {
@@ -144,7 +216,7 @@ function UsersList({ setCurrentPath }) {
                                             </Typography>
                                         </Grid>
                                         <Grid xs={2} direction="column">
-                                            <IconButton aria-label="edit" onClick={handleEdit(user)}><EditIcon style={{ color: 'black', fontSize: '24px' }} /></IconButton>
+                                            <IconButton aria-label="edit" onClick={() => showEditDialog(user)}><EditIcon style={{ color: 'black', fontSize: '24px' }} /></IconButton>
                                             <IconButton aria-label="delete" onClick={() => handleDelete(user.email)}><DeleteIcon style={{ color: 'black', fontSize: '24px' }} /></IconButton>
                                         </Grid>
                                     </Grid>
@@ -169,7 +241,7 @@ function UsersList({ setCurrentPath }) {
                                         </Typography>
                                     </Grid>
                                     <Grid xs={2} direction="column">
-                                        <IconButton aria-label="edit" onClick={handleEdit(data)}><EditIcon style={{ color: 'black', fontSize: '24px' }} /></IconButton>
+                                        <IconButton aria-label="edit" onClick={() => showEditDialog(data)}><EditIcon style={{ color: 'black', fontSize: '24px' }} /></IconButton>
                                     </Grid>
                                 </Grid>
                             </Item>
@@ -177,6 +249,50 @@ function UsersList({ setCurrentPath }) {
                     }
                 </Grid>
             </Box>
+            <Dialog open={openEditDialog} onClose={handleEditClose}>
+                <DialogTitle>Edit User data</DialogTitle>
+                <Box sx={{ display: 'flex', alignItems: 'center', padding: '10px 15px' }}>
+                    <TextField
+                        id="input-with-name"
+                        label="Name"
+                        defaultValue={userOldInfo ? userOldInfo['userName'] : 'Enter your Name'}
+                        variant="outlined"
+                        onChange={(e) => setUserNewName(e.target.value)} />
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', padding: '0 15px' }}>
+                    <TextField
+                        id="input-with-email"
+                        label="Email"
+                        defaultValue={userOldInfo ? userOldInfo['email'] : 'Enter your Email'}
+                        variant="outlined"
+                        onChange={(e) => setUserNewEmail(e.target.value)} />
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', padding: '10px 15px' }}>
+                    <TextField
+                        id="input-with-password"
+                        label="Enter your password"
+                        variant="outlined"
+                        onChange={(e) => setUserNewPassword(e.target.value)} />
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', padding: '10px 15px' }}>
+                    <TextField
+                        id="input-with-authority"
+                        label="Authority"
+                        defaultValue={userOldInfo ? userOldInfo['role'] : 'user'}
+                        variant="outlined"
+                        onChange={(e) => setUserNewRole(e.target.value)} />
+                </Box>
+                <DialogActions>
+                    <Button onClick={handleEditClose}>Cancel</Button>
+                    <Button onClick={() => handleEditSave(userOldInfo)}>Update</Button>
+                </DialogActions>
+                {postResult &&
+                    <div className="alert alert-secondary mt-2" role="alert">
+                        <ErrorIcon fontSize='larger' />
+                        <pre>{postResult}</pre>
+                    </div>
+                }
+            </Dialog>
         </>
     );
 }
